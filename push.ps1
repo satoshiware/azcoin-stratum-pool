27117129f835fe06d9d339690303005dba865a5d
bcraypo <bcraypo@gmail.com>
2026-03-23 15:44:15 -0700

    Creating Updated V0.1.4

--- push.ps1
+++ push.ps1
@@ -1,6 +1,6 @@
 git status
 git add -A
-git commit -m "Creating Updated V0.1.2"
-git tag v0.1.2
+git commit -m "Creating Updated V0.1.4"
+git tag v0.1.4
 git push origin main
-git push origin v0.1.2
+git push origin v0.1.4
